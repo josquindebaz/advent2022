6274a3d10604787ed50c07f25bd78abd522deb50
Josquin Debaz <josquindebaz@protonmail.com>
2022-12-02 14:28:00 +0100

    second half of day 1 puzzle

--- advent2022/day1/RichestElf.cs
+++ advent2022/day1/RichestElf.cs
@@ -6,20 +6,25 @@ public class RichestElf
     {
         var lines = File.ReadLines("./day1/input.txt");
 
-        long richestElf = 0;
+        // long richestElf = 0;
         long summedCalories = 0;
-
+        var richestElves = new long[4];  
+            
         foreach (var line in lines)
         {
             if (long.TryParse(line, out var calories))
             {
                 summedCalories += calories;
+                richestElves[0] = summedCalories;
+                Array.Sort(richestElves);
                 continue;
             }
-            richestElf = Math.Max(richestElf, summedCalories);
+            // richestElf = Math.Max(richestElf, summedCalories);
             summedCalories = 0;
         }
-
-        Console.WriteLine(richestElf);
+        
+        Console.WriteLine($"first half of the puzzle {richestElves[3]}");
+        // Console.WriteLine($"first half of the puzzle {richestElf}");
+        Console.WriteLine($"second half of the puzzle {richestElves[1..].Sum()}");
     }
 }
